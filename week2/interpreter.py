7c036f592d6216d6d9f3f01de5c3372b776db9b0
joeduclk <3669308367@qq.com>
2026-03-11 16:05:54 +0800

    interpreter.py

--- week2/interpreter.py
+++ week2/interpreter.py
@@ -1,2 +1,19 @@
 def main():
-    expression=input
+    expression=input("Expression").strip()
+    x,s,y=expression.split()
+    x_str=float(x)
+    y_str=float(y)
+    out=calucator(x_str,s,y_str)
+    print(out)
+
+
+def calucator(x_str,s,y_str):
+    if s=="+":
+        return float(x_str+y_str)
+    elif s=="-":
+        return float(x_str-y_str)
+
+
+
+
+main()
